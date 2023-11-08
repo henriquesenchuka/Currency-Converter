6cc1e5eda2fcf151ce48153d30abc6857f6bc414
henriquesenchuka <henrique.ique06@gmail.com>
2023-11-08 20:28:25 -0300

    Added other currency options

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const dollarToday = 4.89
 const libraToday = 6.01
-const euroToday = 5.24
+const euroToday = 5.26
 const bitCoinToday = 173000.93
 
 const convertButton = document.querySelector(".convert-button")
@@ -96,6 +96,154 @@ function convertAmount() {
             const convertedValue = amountValue / 1
 
             convertFromParagraph.innerHTML = new Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(convertedValue)
+        }
+
+
+
+        if (convertFromValue == "dollar" && convertForValue == "dollar") {
+            const convertedValue = amountValue / 1
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "dollar" && convertForValue == "euro") {
+            const convertedValue = amountValue * dollarToday / euroToday 
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "EUR"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "dollar" && convertForValue == "libra") {
+            const convertedValue = amountValue * dollarToday / libraToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "GBP"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "dollar" && convertForValue == "bc") {
+            const convertedValue = amountValue * dollarToday / bitCoinToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "BTC"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "dollar" && convertForValue == "real") {
+            const convertedValue = amountValue * dollarToday / 1
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(convertedValue)
+        }
+
+
+
+        if (convertFromValue == "euro" && convertForValue == "dollar") {
+            const convertedValue = amountValue * euroToday / dollarToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "EUR"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "euro" && convertForValue == "euro") {
+            const convertedValue = amountValue * euroToday / euroToday 
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "EUR"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "EUR"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "euro" && convertForValue == "libra") {
+            const convertedValue = amountValue * euroToday / libraToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "EUR"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "GBP"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "euro" && convertForValue == "bc") {
+            const convertedValue = amountValue * euroToday / bitCoinToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "EUR"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "BTC"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "euro" && convertForValue == "real") {
+            const convertedValue = amountValue * euroToday / 1
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "EUR"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(convertedValue)
+        }
+
+
+
+        if (convertFromValue == "libra" && convertForValue == "dollar") {
+            const convertedValue = amountValue * libraToday / dollarToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "GBP"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "libra" && convertForValue == "euro") {
+            const convertedValue = amountValue * libraToday / euroToday 
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "GBP"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "EUR"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "libra" && convertForValue == "libra") {
+            const convertedValue = amountValue * libraToday / libraToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "GBP"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "GBP"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "libra" && convertForValue == "bc") {
+            const convertedValue = amountValue * libraToday / bitCoinToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "GBP"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "BTC"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "libra" && convertForValue == "real") {
+            const convertedValue = amountValue * libraToday / 1
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "GBP"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(convertedValue)
+        }
+
+
+
+        if (convertFromValue == "bc" && convertForValue == "dollar") {
+            const convertedValue = amountValue * bitCoinToday / dollarToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "BTC"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "bc" && convertForValue == "euro") {
+            const convertedValue = amountValue * bitCoinToday / euroToday 
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "BTC"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "EUR"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "bc" && convertForValue == "libra") {
+            const convertedValue = amountValue * bitCoinToday / libraToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "BTC"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("uk-UK", {style: "currency", currency: "GBP"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "bc" && convertForValue == "bc") {
+            const convertedValue = amountValue * bitCoinToday / bitCoinToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "BTC"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "BTC"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "bc" && convertForValue == "real") {
+            const convertedValue = amountValue * bitCoinToday / 1
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "BTC"}).format(amountValue)
             convertForParagraph.innerHTML = new Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(convertedValue)
         }
 }
