41b66f94c7be8f88a5f8835afd2c406e120ea783
henriquesenchuka <henrique.ique06@gmail.com>
2023-11-08 14:59:12 -0300

    JavaScript enhancements

--- script.js
+++ script.js
@@ -4,17 +4,32 @@ const euroToday = 5.24
 const bitCoinToday = 173000.93
 
 const convertButton = document.querySelector(".convert-button")
+const currencySelectFrom = document.querySelector("#convert-from")
+const currencySelectFor = document.querySelector("#convert-for")
 
 
 function convertAmount() {
     const amountValue = document.querySelector("#amount").value // Amount Input Value
+    const convertFromValue = currencySelectFrom.value
+    const convertForValue = currencySelectFor.value
     const convertFromParagraph = document.querySelector(".from-amount-p")
     const convertForParagraph = document.querySelector(".for-amount-p")
 
-    const convertedValue = amountValue / dollarToday
+        if (convertFromValue == "real" && convertForValue == "dollar") {
+            const convertedValue = amountValue / dollarToday
 
-    convertFromParagraph.innerHTML = new Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(amountValue)
-    convertForParagraph.innerHTML = new Intl.NumberFormat("en", {style: "currency", currency: "USD"}).format(convertedValue)
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(convertedValue)
+        }
+
+        if (convertFromValue == "real" && convertForValue == "euro") {
+            const convertedValue = amountValue / dollarToday
+
+            convertFromParagraph.innerHTML = new Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(amountValue)
+            convertForParagraph.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(convertedValue)
+        }
+
+
 }
 
 
